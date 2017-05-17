2e9ee382942455c84f7fd13917ca03ba9d87bfa9
Stijn Verhoeven <stijn.verhoeven@inovativ.be>
2017-05-17 23:22:40 +0200

    init

--- HPDemo/Migrations/201705160833056_initial.cs
+++ HPDemo/Migrations/201705160833056_initial.cs
@@ -7,22 +7,10 @@ namespace HPDemo.Migrations
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.Movies",
-                c => new
-                {
-                    ID = c.Int(nullable: false, identity: true),
-                    Title = c.String(),
-                    ReleaseDate = c.DateTime(nullable: false),
-                    Genre = c.String(),
-                    Price = c.Decimal(nullable: false, precision: 18, scale: 2),
-                })
-                .PrimaryKey(t => t.ID);
         }
         
         public override void Down()
         {
-            DropTable("dbo.Movies");
         }
     }
 }
